52beab4397205fcbed5ef35207b15fe195621500
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-06 19:15:29 -0300

    Add button and wallet functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
 
@@ -7,6 +7,53 @@ const TWITTER_HANDLE = "web3dev_"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState(null)
+
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Eu acho que você não tem a metamask!");
+        return;
+      } else {
+        console.log("Nós temos o objeto ethereum", ethereum);
+      }
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Carteira conectada::", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("Não encontramos uma carteira conectada");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Instale o Metamask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Conectado", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() =>{
+    checkIfWalletIsConnected();
+  },[]);
+
   return (
     <div className="App">
       <div className="container">
@@ -15,9 +62,15 @@ const App = () => {
           <p className="sub-text">Junte-se a mim para vencer os inimigos do Metaverso!</p>
           <div className="connect-wallet-container">
             <img
-              src="https://thumbs.gfycat.com/AnchoredPleasedBergerpicard-size_restricted.gif"
-              alt="Nascimento Gif"
+              src="https://media.giphy.com/media/PJvvewo5mTtXW/giphy.gif"
+              alt="Samurai Gif"
             />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Conecte sua carteira para começar
+            </button>
           </div>
         </div>
         <div className="footer-container">
