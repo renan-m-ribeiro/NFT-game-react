b7e89eef197cd671b8f060c85c9079d097da3bcb
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-07 23:27:52 -0300

    Create Component

---
+++ src/Components/SelectCharacter/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./SelectCharacter.css";
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+    return (
+        <div className="select-character-container">
+            <h2>Minte seu Herói. Escolha com sabedoria.</h2>
+        </div>
+    );
+};
+
+export default SelectCharacter;
